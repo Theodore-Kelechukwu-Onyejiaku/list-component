b6ae94e92a679d3de2f749043dd3aa9c7b1dcb56
Theodore-Kelechukwu-Onyejiaku <theodoreonyejiaku@gmail.com>
2020-06-01 17:09:16 -0800

    format code properly

--- src/App.js
+++ src/App.js
@@ -1,26 +1,30 @@
 import React, { useState } from "react";
 import "bootstrap/dist/css/bootstrap.css";
 import "./App.css";
-import genie from "./genie.png"
-
-
-
+import genie from "./genie.png";
 
 const OurServices = ({ each }) => {
   return <li className="list-group-item list-group-item-success">{each}</li>;
 };
 
 const ClientServices = ({ each }) => {
-  return <li className="list-group-item" style={{"backgroundColor":"rgb(197, 21, 21)", "color": "white"}}>{each}</li>;
+  return (
+    <li
+      className="list-group-item"
+      style={{ backgroundColor: "rgb(197, 21, 21)", color: "white" }}
+    >
+      {each}
+    </li>
+  );
 };
 
 const SelectServices = ({ each }) => {
   return <option value={each}> {each}</option>;
 };
 
-const DeleteServices = ({each})=>{
-  return <option value= {each}>{each}</option>
-}
+const DeleteServices = ({ each }) => {
+  return <option value={each}>{each}</option>;
+};
 
 const App = () => {
   const [services, setServices] = useState([
@@ -32,7 +36,7 @@ const App = () => {
     "email",
     "chat bots",
     "text",
-    "iot devices"
+    "iot devices",
   ]);
   const [customerServices, setCustomerServices] = useState([]);
   const [addServiceValue, setAddServiceValue] = useState("");
@@ -75,11 +79,18 @@ const App = () => {
     <div className="App text-danger container">
       <div className="heading text-center">
         <h1>Welcome to Support Genie</h1>
-        <img  src={genie} className="img-fluid" style={{"max-width": "100%","height": "auto" }}/>
+        <img
+          src={genie}
+          className="img-fluid"
+          style={{ "max-width": "100%", height: "auto" }}
+        />
       </div>
       <div className="row mt-3">
         <div className="col-sm-12 col-md-4 col-lg-4 col-xl-4">
-          <ul className="list-group" style={{"border": "3px rgb(197, 21, 21) solid"}}>
+          <ul
+            className="list-group"
+            style={{ border: "3px rgb(197, 21, 21) solid" }}
+          >
             <p className=" text-white text-center">
               Our Core Services essentil for 21st Century Customer Care Services
             </p>
@@ -127,7 +138,10 @@ const App = () => {
           </div>
         </div>
         <div className="col-sm-12 col-md-4 col-lg-4 col-xl-4">
-          <ul className="list-group" style={{"border": "3px rgb(197, 21, 21) solid"}}>
+          <ul
+            className="list-group"
+            style={{ border: "3px rgb(197, 21, 21) solid" }}
+          >
             <p className="text-white text-center">
               Services Every Customer Service Provider Should Have
             </p>
@@ -137,13 +151,22 @@ const App = () => {
           </ul>
         </div>
       </div>
-      <hr/>
+      <hr />
       <footer class="footer mt-5">
-          <div class="container text-center">
-            <span class="text-white">Created By Theodore Kelechukwu Onyejiaku 
-            <br/><a href="https://www.linkedin.com/in/theodore20151014166/"><i className="fa fa-linkedin text-white"></i></a> | <a href="https://github.com/Theodore-Kelechukwu-Onyejiaku"><i className="fa fa-github text-white"></i></a></span>
-          </div>
-        </footer>
+        <div class="container text-center">
+          <span class="text-white">
+            Created By Theodore Kelechukwu Onyejiaku
+            <br />
+            <a href="https://www.linkedin.com/in/theodore20151014166/">
+              <i className="fa fa-linkedin text-white"></i>
+            </a>{" "}
+            |{" "}
+            <a href="https://github.com/Theodore-Kelechukwu-Onyejiaku">
+              <i className="fa fa-github text-white"></i>
+            </a>
+          </span>
+        </div>
+      </footer>
     </div>
   );
 };
